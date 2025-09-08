f1ab3b5968d6f4b074e55609d199a1b1f57c97f1
Brook <brook-tamrat.kifle@univ-rouen.fr>
2025-09-08 20:31:12 +0200

    validation question 4.6

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -3,8 +3,8 @@ print("Hello, World!", "\n")
 import fonctions as f
 
 while True:
-	nbr=(int, float)(input("Entree un nombre : "))
-	puissance=(int, float)(input("Entree sa puissance : "))
+	nbr=float(input("Entree un nombre : "))
+	puissance=float(input("Entree sa puissance : "))
 
 	res = f.puissance_nombre(nbr,puissance)
 	print(nbr,"**",puissance,"=", res, "\n")
