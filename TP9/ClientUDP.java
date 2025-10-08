1286952941fccbe7fbad52d539d6637336e3f663
KIFLE <k.brooktamrat@gmail.com>
2025-10-08 16:02:41 +0200

    UDP

---
+++ TP9/ClientUDP.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientUDP 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        InetAddress addr = InetAddress.getLocalHost();
+        System.out.println("adresse=" + addr.getHostName());
+
+        String s = "Bonjour UDP";
+        byte[] data = s.getBytes();
+
+        DatagramPacket packet = new DatagramPacket(data, data.length, addr, 1234);
+        DatagramSocket sock = new DatagramSocket();
+        sock.send(packet);
+        sock.receive(packet);
+        String str = new String(packet.getData());
+        System.out.println("recu=" + str);
+        sock.close();
+    }
+}
+
